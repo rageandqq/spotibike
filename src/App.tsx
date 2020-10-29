f25c788134d70dc91baa038c4179943416588731
Sameer Chitley <chitley.sameer@gmail.com>
2020-10-28 23:18:06 -0700

    Project Initialized
    Initialized with create-react-app, react-router-dom and typescript.
    Setup basic routing structure as.

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+// import React from 'React';
+import './App.css';
+import Splash from './SplashScreen';
+import Login from './Login';
+
+import * as React from "react";
+
+
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Splash</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route path="/login"><Login/></Route>
+        <Route path="/"><Splash /></Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/SplashScreen.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function Splash() {
+  return (
+    <div>
+      <header className="Splash-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
