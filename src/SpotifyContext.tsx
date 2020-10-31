f2ff085c5bde5fe87bcdf4a77d976146c1fd8bc3
Sameer Chitley <chitley.sameer@gmail.com>
2020-10-31 00:57:03 -0700

    Updates to spotify auth; prevent multiple refetches, also fetch user ID in subsequent query

--- src/SpotifyContext.tsx
+++ src/SpotifyContext.tsx
@@ -4,12 +4,14 @@ interface SpotifyContext {
   expiresAt: number | null; // epoch time this expires at
   isAuth: boolean;
   api: any; // TODO figure out how tf to do this with typescript
+  userID: string | null;
 }
 
 const _default: SpotifyContext = {
   expiresAt: null,
   isAuth: false,
   api: null,
+  userID: null,
 };
 
 export default React.createContext(_default);
--- src/SpotifyProvider.tsx
+++ src/SpotifyProvider.tsx
@@ -1,16 +1,31 @@
 import * as React from "react";
 import SpotifyContext from "./SpotifyContext";
 
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect, useCallback, useRef } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 
 type IProps = {
   children: React.ReactNode;
 };
 
+function usePrevious<T>(value: T) {
+  // The ref object is a generic container whose current property is mutable ...
+  // ... and can hold any value, similar to an instance property on a class
+  const ref = useRef<T>();
+
+  // Store current value in ref
+  useEffect(() => {
+    ref.current = value;
+  }, [value]); // Only re-run if value changes
+
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
+}
+
 const api = new SpotifyWebApi();
 
 export default function SpotifyProvider({ children }: IProps) {
+  const [userID, setUserID] = useState<null | string>(null);
   const hashParams = new URLSearchParams(
     window.location.hash.substr(1) // skip the first char (#)
   );
@@ -25,20 +40,39 @@ export default function SpotifyProvider({ children }: IProps) {
     expiresInFromAuth != null ? +expiresInFromAuth : null // not a typo, use "+" to convert to number
   );
 
+  const initAPI = useCallback((token) => {
+    api.setAccessToken(token);
+    api.getMe().then((data) => {
+      setUserID(data.id);
+    });
+  }, []);
+
   // This is probbaly unnecessary... but if it changes, we should do something
+  const prevAccessTokenFromAuth = usePrevious<string | null>(
+    accessTokenFromAuth
+  );
+  const prevExpiresInFromAuth = usePrevious<string | null>(expiresInFromAuth);
   useEffect(() => {
-    if (accessTokenFromAuth != null) {
+    if (
+      accessTokenFromAuth != null &&
+      prevAccessTokenFromAuth !== accessTokenFromAuth
+    ) {
       setAccessToken(accessTokenFromAuth);
-      api.setAccessToken(accessTokenFromAuth);
+      initAPI(accessTokenFromAuth);
     }
-    if (expiresInFromAuth != null) {
+    if (
+      expiresInFromAuth != null &&
+      prevExpiresInFromAuth !== expiresInFromAuth
+    ) {
       setExpiresAt(new Date().getTime() + +expiresInFromAuth); // not a typo, use "+" to convert to number
     }
-  }, [accessTokenFromAuth, expiresInFromAuth]);
-
-  if (accessTokenFromAuth != null) {
-    api.setAccessToken(accessTokenFromAuth);
-  }
+  }, [
+    prevAccessTokenFromAuth,
+    accessTokenFromAuth,
+    prevExpiresInFromAuth,
+    expiresInFromAuth,
+    initAPI,
+  ]);
 
   const providerValue = useMemo(
     () => ({
@@ -46,8 +80,9 @@ export default function SpotifyProvider({ children }: IProps) {
       expiresAt,
       isAuth: accessToken != null, // TODO: figure out how to use expiresAt
       api,
+      userID,
     }),
-    [accessToken, expiresAt]
+    [accessToken, expiresAt, userID]
   );
 
   return (
